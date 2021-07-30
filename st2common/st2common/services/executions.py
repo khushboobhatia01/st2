45805e10ab2cd96843628095c19ba6083799f673
Khushboo <bkhushboo@vmware.com>
2021-07-30 10:49:26 +0530

    Initial commit

--- st2common/st2common/services/executions.py
+++ st2common/st2common/services/executions.py
@@ -175,6 +175,14 @@ def _get_web_url_for_execution(execution_id):
 
 def update_execution(liveaction_db, publish=True):
     execution = ActionExecution.get(liveaction__id=str(liveaction_db.id))
+
+    # Skip execution object update when action is already in completed state.
+    if execution.status in action_constants.LIVEACTION_COMPLETED_STATES:
+        LOG.debug(
+            "[%s] Action is already in completed state: %s. Skipping execution update to state: %s."
+            % (execution.id, execution.status, liveaction_db.status)
+        )
+        return execution
     decomposed = _decompose_liveaction(liveaction_db)
 
     kw = {}
